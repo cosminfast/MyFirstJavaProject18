f905664ff57a8cbec24a59ff4dbb8d34a61aa23b
cosminciocan <ciocan.cosmin@gmail.com>
2019-12-19 20:29:38 +0200

    tema lab 1aaa

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
         int x;
 
         x = 2;
@@ -24,8 +24,6 @@ public class Main {
         System.out.println(x);
 
 
-        int aaaa = 33123;
-        System.out.println(aaaa);
     }
 
 }
