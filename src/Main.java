09ee8da3122322c079dfe925b798013160606ca3
cosminciocan <ciocan.cosmin@gmail.com>
2019-12-19 20:29:03 +0200

    tema lab 1

--- src/Main.java
+++ src/Main.java
@@ -23,6 +23,9 @@ public class Main {
 //        System.out.println("suma lui varsta si y este: " + (varsta + y));
         System.out.println(x);
 
+
+        int aaaa = 33123;
+        System.out.println(aaaa);
     }
 
 }
