ff3d0ba670cc71972fa278c57ed20871109d1e9d
cosminciocan <ciocan.cosmin@gmail.com>
2019-12-19 20:26:32 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        int x;
+
+        x = 2;
+
+        short varsta = 12;
+
+        float y = 4.5f;
+
+        float sum = varsta + y;
+
+        String myString = "Cosmin are " + varsta + " ani";
+
+//        int rez = x + 2 + ++x;
+
+        x+=x+=x++ + ++x;
+
+
+//        System.out.println("suma lui varsta si y este: " + (varsta + y));
+        System.out.println(x);
+
+    }
+
+}
